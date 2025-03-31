8740612608218692baf2aa2ba22c724fedc4a49c
puppy09 <zepedaramireztics@gmail.com>
2025-03-31 15:01:12 -0700

    modelos

--- models/versiones.js
+++ models/versiones.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../database.js');
-
+const Proyectos = require('../models/proyectos.js');
+const Diagrama = require('../models/diagramas.js');
 class Versiones extends Model{}
 Versiones.init({
     id_version: {
@@ -10,11 +11,19 @@ Versiones.init({
     },
     id_proyecto:{
         type:DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        references:{
+            model: 'proyectos',
+            keys: 'id_proyecto'
+        }
     },
-    id_Tipodiagrama:{
+    diagrama:{
         type:DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        references:{
+            model: 'diagrama',
+            keys: 'id_diagrama'
+        }
     },
     isActive: {
         type: DataTypes.BOOLEAN,
@@ -37,4 +46,17 @@ Versiones.init({
     timestamps: false
 });
 
+Versiones.associate = function(models) {
+    // A Versiones belongs to a Proyectos
+    Versiones.belongsTo(models.Proyectos, {
+        foreignKey: 'id_proyecto',
+        as: 'proyecto' // Alias to use when fetching associated data
+    });
+
+    // A Versiones belongs to a Diagramas
+   /* Versiones.belongsTo(models.diagrama, {
+        foreignKey: 'id_diagrama',
+        as: 'diagrama' // Alias to use when fetching associated data
+    });*/
+};
 module.exports = Versiones;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const versionService = require('../services/versionService');
+const { crearProyecto, getProyectos, updaProyecto } = require('../controllers/proyectos');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Proyectos
+router.post('/crearproyecto', crearProyecto);
+router.get('/proyectos', getProyectos);
+router.put('/proyecto/:id_proyecto', updaProyecto);
 module.exports = router;
