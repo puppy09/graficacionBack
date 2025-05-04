03668bfb2ed63788f7feea66181e41e98c960172
YunAtondo <pcu18022243@gmail.com>
2025-05-04 15:04:04 -0700

    ya hace del proyecto de angular osea el front algunos archivos
    Todos los archivos de modificacion y los archivos referentes al login, register y main menu aunque vacio

--- services/projectService.js
+++ services/projectService.js
@@ -37,9 +37,6 @@ const createProject = async (nombreProyecto, graphModel, credenciales) => {
         fs.mkdirSync(projectFolderPath, {recursive: true});
         console.log(`Carpeta del proyecto creada en ${projectFolderPath}`);
     
-        // Crear proyecto frontend
-        //console.log('Creando proyecto frontend en angular...');
-        //await executeCommand(`npx -y @angular/cli new ${nombreProyecto}-frontend --defaults`, projectFolderPath);
     
         // Crear proyecto backend
         console.log('Creando proyecto backend en express...');
@@ -58,6 +55,9 @@ const createProject = async (nombreProyecto, graphModel, credenciales) => {
         //instalar express-session
         console.log('instalando express-session...');
         await executeCommand('npm install express-session', backendPath);
+        //instalar cors
+        console.log('instalando cors...');
+        await executeCommand('npm install cors', backendPath);
 
 
         // Crear la carpeta models en el backend
@@ -118,6 +118,8 @@ sequelize.sync({ force: true }).then(() => {
             console.log('Modificaciones agregadas a app.js');
          // Código a insertar después de "var app = express();"
     const sessionConfig = `
+const cors = require('cors');
+app.use(cors({ credentials: true, origin: true }));
     const session = require('express-session');
     app.use(session({
         secret: process.env.SECRET || 'default_secret',
@@ -142,6 +144,10 @@ sequelize.sync({ force: true }).then(() => {
         console.log('Proyecto creado correctamente');
 
         processGraphModel(graphModel, modelsPath, routesPath, controllersPath, middlewaresPath);
+        // Crear proyecto frontend
+        console.log('Creando proyecto frontend en angular...');
+        await executeCommand(`npx -y @angular/cli new ${nombreProyecto}-frontend --defaults`, projectFolderPath);
+        crearArchivosFrontend(frontendPath);
     }
 };
 
@@ -749,7 +755,524 @@ const mapSequelizeType = (type) => {
     }
 };
 
+const crearArchivosFrontend = async (frontendPath) => {
+    const srcPath = path.join(frontendPath, 'src');
+    const appComponentPath = path.join(srcPath, 'app', 'app.component.html');
+    const appConfigPath = path.join(srcPath, 'app', 'app.config.ts');
+    const componentsFolderPath = path.join(srcPath, 'app', 'components');
+    const servicesFolderPath = path.join(srcPath, 'app', 'services');
+    const apiLinkPath = path.join(srcPath, 'app', 'apiLink.ts');
 
+    fs.mkdirSync(componentsFolderPath, { recursive: true });
+    fs.mkdirSync(servicesFolderPath, { recursive: true });
+    const apiLinkContent = `
+    export const apiUrl = 'http://localhost:3000'; 
+    `
+    fs.writeFileSync(apiLinkPath, apiLinkContent, 'utf8');
+
+    fs.writeFileSync(appConfigPath, '', 'utf8');
+    const appConfigContent = `
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(withFetch(),withInterceptorsFromDi())]
+};
+
+    `
+    fs.writeFileSync(appConfigPath, appConfigContent, 'utf8');
+    
+    fs.writeFileSync(appComponentPath, '', 'utf8');
+    const appComponentContent = `
+    <router-outlet></router-outlet>
+    `
+    fs.writeFileSync(appComponentPath, appComponentContent, 'utf8');
+    const appRoutesPath = path.join(srcPath, 'app', 'app.routes.ts');
+    fs.writeFileSync(appRoutesPath, '', 'utf8');
+    const appRoutesContent = `
+    import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { RegisterComponent } from './components/register/register.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+    {
+        path: 'mainMenu',
+        component: MainMenuComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+];
+
+    `
+    fs.writeFileSync(appRoutesPath, appRoutesContent, 'utf8');
+
+    await generarComponentesBase(componentsFolderPath);
+    await generarServiciosBase(servicesFolderPath);
+
+    
+}
+
+generarComponentesBase = async (componentsFolderPath) => {
+    
+    await executeCommand('ng g c login', componentsFolderPath);
+    await executeCommand('ng g c register', componentsFolderPath);
+    await executeCommand('ng g c mainMenu', componentsFolderPath);
+
+    const loginComponentPath = path.join(componentsFolderPath, 'login');
+    const mainMenuComponentPath = path.join(componentsFolderPath, 'main-menu');
+    const registerComponentPath = path.join(componentsFolderPath, 'register');
+
+    const loginComponentHtmlPath = path.join(loginComponentPath, 'login.component.html');
+    const loginComponentTsPath = path.join(loginComponentPath, 'login.component.ts');
+    const loginComponentCssPath = path.join(loginComponentPath, 'login.component.css');
+
+    const registerComponentHtmlPath = path.join(registerComponentPath, 'register.component.html');
+    const registerComponentTsPath = path.join(registerComponentPath, 'register.component.ts');
+    const registerComponentCssPath = path.join(registerComponentPath, 'register.component.css');
+
+    const mainMenuComponentHtmlPath = path.join(mainMenuComponentPath, 'main-menu.component.html');
+    const mainMenuComponentTsPath = path.join(mainMenuComponentPath, 'main-menu.component.ts');
+    const mainMenuComponentCssPath = path.join(mainMenuComponentPath, 'main-menu.component.css');
+
+    const loginComponentHtmlContent = `
+    <div class="login-container">
+    <div class="login-box">
+        <div class="login-header">
+            <p class="welcome-text">Bienvenido a tu proyecto generado</p>
+            <div class="register-link">
+                <p>No tienes una cuenta? <span class="register-text" (click)="goToRegisterUser()">Regístrate</span></p>
+            </div>
+        </div>
+
+        <h1 class="login-title">Iniciar sesión</h1>
+        
+        <form onsubmit="onSubmit()" class="login-form">
+            <div class="form-group">
+                <label for="email" class="form-label">Ingresa tu email</label>
+                <input type="text" id="email" [(ngModel)]="email" name="email" placeholder="email" class="form-input" />
+            </div>
+            
+            <div class="form-group">
+                <label for="password" class="form-label">Ingresa tu contraseña</label>
+                <input type="password" id="password" [(ngModel)]="password" name="password" placeholder="Contraseña" class="form-input" />
+            </div>
+            
+            <button type="submit" class="submit-button" (click)="onSubmit()">Iniciar Sesión</button>
+        </form>
+    </div>
+</div>
+    `
+    fs.writeFileSync(loginComponentHtmlPath, loginComponentHtmlContent, 'utf8');
+    const loginComponentTsContent = `
+    import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule,ReactiveFormsModule,FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(private userService: UserService, private router: Router ){}
+  ngOnInit(): void {
+    
+  }
+  onSubmit(){
+    const loginData = {
+      email: this.email,
+      password: this.password
+    };
+    this.userService.login(loginData).subscribe(
+      (response) => {
+        console.log('Login successful', response);
+        // Navigate to the desired route after successful login
+        this.router.navigate(['/mainMenu']);
+      },
+      (error) => {
+        alert('Error en el login, revisa tus credenciales');
+        console.error('Login error', error);
+        // Handle login error here (e.g., show an error message)
+      }
+    );
+  }
+
+  goToRegisterUser(){
+    this.router.navigate(['/register']);
+  }
+
+}
+    `
+    fs.writeFileSync(loginComponentTsPath, loginComponentTsContent, 'utf8');
+    const loginComponentCssContent = `
+    .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f3f4f6;
+    height: 100vh;
+}
+
+/* Login box styles */
+.login-box {
+    background-color: #ffffff;
+    width: 100%;
+    max-width: 400px;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+/* Header styles */
+.login-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+
+.welcome-text {
+    font-size: 18px;
+    font-family: 'Arial', sans-serif;
+    color: #000000;
+}
+
+.register-link {
+    text-align: right;
+    font-size: 12px;
+    color: #6b7280;
+}
+
+.register-text {
+    color: #8644b3;
+    cursor: pointer;
+    font-weight: bold;
+}
+
+/* Title styles */
+.login-title {
+    font-size: 28px;
+    font-weight: bold;
+    margin: 20px 0;
+    color: #000000;
+    font-family: 'Arial', sans-serif;
+}
+
+/* Form styles */
+.login-form {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+
+.form-group {
+    display: flex;
+    flex-direction: column;
+}
+
+.form-label {
+    font-size: 14px;
+    font-family: 'Arial', sans-serif;
+    color: #4b5563;
+    margin-bottom: 5px;
+}
+
+.form-input {
+    padding: 10px;
+    border: 1px solid #d1d5db;
+    border-radius: 8px;
+    font-size: 14px;
+    color: #374151;
+    outline: none;
+}
+
+.form-input:focus {
+    border-color: #14b8a6;
+    box-shadow: 0 0 0 2px rgba(20, 184, 166, 0.5);
+}
+
+/* Button styles */
+.submit-button {
+    padding: 10px;
+    background-color: #9ad6ce;
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: bold;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.submit-button:hover {
+    background-color: #14b8a6;
+}
+
+.submit-button:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(20, 184, 166, 0.5);
+}
+    `
+    fs.writeFileSync(loginComponentCssPath, loginComponentCssContent, 'utf8');
+
+//Va lo del register
+    const registerComponentHtmlContent = `
+    <div class="login-container">
+    <div class="login-box">
+        <div class="login-header">
+            <p class="welcome-text">Bienvenido a tu proyecto generado</p>
+            <div class="register-link">
+                <p>Ya tienes una cuenta? <span class="register-text" (click)="goToLogin()">Inicia sesión</span></p>
+            </div>
+        </div>
+
+        <h1 class="login-title">Registrar usuario</h1>
+        
+        <form onsubmit="onSubmit()" class="login-form">
+            <div class="form-group">
+                <label for="email" class="form-label">Ingresa tu email</label>
+                <input type="text" id="email" [(ngModel)]="email" name="email" placeholder="email" class="form-input" />
+            </div>
+            
+            <div class="form-group">
+                <label for="password" class="form-label">Ingresa tu contraseña</label>
+                <input type="password" id="password" [(ngModel)]="password" name="password" placeholder="Contraseña" class="form-input" />
+            </div>
+            
+            <button type="submit" class="submit-button" (click)="onSubmit()">Iniciar Sesión</button>
+        </form>
+    </div>
+</div>
+    `
+    fs.writeFileSync(registerComponentHtmlPath, registerComponentHtmlContent, 'utf8');
+    const registerComponentTsContent = `
+    import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule,ReactiveFormsModule,FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+  email = '';
+  password = '';
+
+  constructor(private userService: UserService, private router: Router ){}
+  ngOnInit(): void {
+    
+  }
+  onSubmit(){
+    const loginData = {
+      email: this.email,
+      password: this.password
+    };
+    this.userService.register(loginData).subscribe(
+      (response) => {
+        console.log('Login successful', response);
+        // Navigate to the desired route after successful login
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        alert('Error en el login, revisa tus credenciales');
+        console.error('Login error', error);
+        // Handle login error here (e.g., show an error message)
+      }
+    );
+  }
+
+  goToLogin(){
+    this.router.navigate(['/']);
+  }
+}
+    `
+    fs.writeFileSync(registerComponentTsPath, registerComponentTsContent, 'utf8');
+    const registerComponentCssContent = `
+    .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f3f4f6;
+    height: 100vh;
+}
+
+/* Login box styles */
+.login-box {
+    background-color: #ffffff;
+    width: 100%;
+    max-width: 400px;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+/* Header styles */
+.login-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+
+.welcome-text {
+    font-size: 18px;
+    font-family: 'Arial', sans-serif;
+    color: #000000;
+}
+
+.register-link {
+    text-align: right;
+    font-size: 12px;
+    color: #6b7280;
+}
+
+.register-text {
+    color: #8644b3;
+    cursor: pointer;
+    font-weight: bold;
+}
+
+/* Title styles */
+.login-title {
+    font-size: 28px;
+    font-weight: bold;
+    margin: 20px 0;
+    color: #000000;
+    font-family: 'Arial', sans-serif;
+}
+
+/* Form styles */
+.login-form {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+
+.form-group {
+    display: flex;
+    flex-direction: column;
+}
+
+.form-label {
+    font-size: 14px;
+    font-family: 'Arial', sans-serif;
+    color: #4b5563;
+    margin-bottom: 5px;
+}
+
+.form-input {
+    padding: 10px;
+    border: 1px solid #d1d5db;
+    border-radius: 8px;
+    font-size: 14px;
+    color: #374151;
+    outline: none;
+}
+
+.form-input:focus {
+    border-color: #14b8a6;
+    box-shadow: 0 0 0 2px rgba(20, 184, 166, 0.5);
+}
+
+/* Button styles */
+.submit-button {
+    padding: 10px;
+    background-color: #9ad6ce;
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: bold;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.submit-button:hover {
+    background-color: #14b8a6;
+}
+
+.submit-button:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(20, 184, 166, 0.5);
+}
+    `
+    fs.writeFileSync(registerComponentCssPath, registerComponentCssContent, 'utf8');
+
+    //Main menu component
+    const mainMenuComponentHtmlContent = `
+    <p>main-menu works!</p>
+    `
+    fs.writeFileSync(mainMenuComponentHtmlPath, mainMenuComponentHtmlContent, 'utf8');
+    const mainMenuComponentTsContent = `
+    import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  imports: [],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css'
+})
+export class MainMenuComponent {
+
+}
+    `
+    fs.writeFileSync(mainMenuComponentTsPath, mainMenuComponentTsContent, 'utf8');
+    const mainMenuComponentCssContent = `
+    
+    `
+    fs.writeFileSync(mainMenuComponentCssPath, mainMenuComponentCssContent, 'utf8');
+    console.log('Componentes generados correctamente');
+}
+
+generarServiciosBase = async (servicesFolderPath) => {
+    await executeCommand('ng g s user', servicesFolderPath);
+    const userServicePath = path.join(servicesFolderPath, 'user.service.ts');
+    const userServiceContent = `
+    import { inject, Injectable } from '@angular/core';
+    import { apiUrl } from '../apiLink';
+    import { HttpClient } from '@angular/common/http';
+    import { Observable } from 'rxjs';
+
+    @Injectable({
+      providedIn: 'root'
+    })
+    export class UserService {
+      private apiUrl = apiUrl; // Aquí no interpolamos
+      private http = inject(HttpClient);
+      constructor() { }
+      login(data: any): Observable<any> {
+        return this.http.post<any>(\`\${this.apiUrl}/login\`, data, {withCredentials: true});
+      }
+      register(data: any): Observable<any> {
+        return this.http.post<any>(\`\${this.apiUrl}/users\`, data, {withCredentials: true});
+      }
+    }
+    `;
+    fs.writeFileSync(userServicePath, userServiceContent.trim(), 'utf8');
+    console.log('Servicios generados correctamente');
+};
 
 module.exports = {
     createProject
