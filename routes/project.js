4bab8db59a9101f40b8983fca2c531108a363866
puppy09 <zepedaramireztics@gmail.com>
2025-05-07 21:56:05 -0700

    ahi va

--- routes/project.js
+++ routes/project.js
@@ -4,6 +4,10 @@ var projectService = require('../services/projectService');
 
 router.post('/crear', async (req, res) => {
     const {nombreProyecto, graphModel, credenciales, paquetesGraph} = req.body;
+    console.log("Nombre de Proyecto",nombreProyecto);
+    console.log("GraphModel", graphModel);
+    console.log("credenciales", credenciales);
+    console.log("paquetes graph",paquetesGraph);
     if (!nombreProyecto, !graphModel) {
         res.status(400).json({error: 'El nombre del proyecto es requerido y/o el modelo de grafos es requerido'});
     }
@@ -11,6 +15,7 @@ router.post('/crear', async (req, res) => {
         await projectService.createProject(nombreProyecto, graphModel, credenciales, paquetesGraph);
         res.json({message: 'Proyecto creado correctamente'});
     } catch (error) {
+        console.log(error);
         res.status(500).json({error: error.message});
     }
     });
